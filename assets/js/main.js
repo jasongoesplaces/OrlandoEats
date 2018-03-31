5dafd9112f4ea86099f32b9783e30e25af79b098
Jason <jasongoesplaces@gmail.com>
2018-03-31 10:50:11 -0400

    added the sign up and log in modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,25 @@
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyDOYpHtFpO_zc0j07ieVWpUT4CjceeEJ1g",
-authDomain: "project1-e5380.firebaseapp.com",
-databaseURL: "https://project1-e5380.firebaseio.com",
-projectId: "project1-e5380",
-storageBucket: "project1-e5380.appspot.com",
-messagingSenderId: "891558406446"
-};
-firebase.initializeApp(config);
-
-
-
-
 // On Document Load
-$(document).ready(() => {
+$(document).ready(function(){
     console.log('Ready');
+
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyDOYpHtFpO_zc0j07ieVWpUT4CjceeEJ1g",
+    authDomain: "project1-e5380.firebaseapp.com",
+    databaseURL: "https://project1-e5380.firebaseio.com",
+    projectId: "project1-e5380",
+    storageBucket: "project1-e5380.appspot.com",
+    messagingSenderId: "891558406446"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database()
+
+    $('.modal').modal();
+
+    $("#submit").on("click", function(event) {
+        event.preventDefault();
+    })
+
     
 })
